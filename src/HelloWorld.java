a963e4ade9370ebe5d82caabb4884aa1f13c04e9
Veronika-C02 <s51857@edu.campus02.at>
2022-09-16 19:12:35 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    //main-Methode
+    public static void main(String[] args) {
+
+        //sout
+        System.out.println("Hello World!");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 8");
+        System.out.println("Zeile 9");
+        System.out.println("Zeile 10");
+        System.out.println("Zeile 11");
+        System.out.println("Zeile 12");
+        System.out.println("Zeile 13");
+        System.out.println("Zeile 14"); //STRG + D (Duplicate
+
+    }
+}
